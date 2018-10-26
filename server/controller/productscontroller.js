89dae58744f15cc0dd38fde41bb8649bf427c702
ITUNUCOLE <itunucole1@gmail.com>
2018-10-26 12:49:51 +0100

    Mocha test

--- server/controller/productscontroller.js
+++ server/controller/productscontroller.js
@@ -1,13 +1,79 @@
-let products = {}
-let product_id = 1;
-//products will be an array of product objects
+const products =[]
 
-export function fetchAllProducts(req, res) {
-    res.json(products);
-}
-export function addProduct(req, res) {
-    req.body.id = product_id;
-    let productId = product_id++;
-    products[productId] = req.body;
-    res.json({ data: products, status: 'success' }, 200);
-}
+//Products will be an array of Products objects
+
+
+class ProductsController{
+
+
+    //Get all Products
+
+
+static getAllProducts (req,res){
+
+        return res.status(200)
+
+        .json({
+
+            status: 'Success',
+
+            products,
+
+        });
+
+    }
+
+
+
+    //Add a Products to the products hash 
+
+    static addProduct (req,res) {
+
+        let product = {
+                 id : productId,
+
+        name : req.body.name,
+
+        price : req.body.price,
+
+        item : req.body.item,
+};
+
+        const productId = products.length + 1;
+
+        products.push(product);
+
+        res.status(200).json({
+
+            message: 'New Product successfully added!',
+
+             status: 'Success',
+
+              product: products[productId - 1],
+
+            });
+
+            req.checkBody("product.name", "Product name must be a text").isAlpha().notEmpty();
+
+            req.checkBody("product.price", "Product name must be a text").isNumber().notEmpty(); 
+
+            req.checkBody("product.item", "Product name must be a text").notEmpty(); 
+
+            
+
+                let errors = req.validationErrors();
+
+                if (errors) {
+
+                    res.send(errors);
+
+                    return;
+
+                } else {
+
+                    //...
+
+                }
+
+            }
+        }
